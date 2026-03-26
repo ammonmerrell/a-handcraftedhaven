2d4eacf9a1b3faecb76ed2b98251450526036541
ammonmerrell <ammonmerrell@outlook.com>
2026-03-26 11:40:14 -0600

    un commented revenue code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,11 +3,11 @@ import styles from "../page.module.css"
 import RevenueChart from '../ui/revinue-chart';
 import LatestInvoices from '../ui/latest-invoices';
 import { quintessential } from '../page.module.css';
-// import { fetchRevenue } from '../lib/data';
+import { fetchRevenue } from '../lib/data';
 // import { fetchLatestInvoices } from '../lib/data';
  
 export default async function Page() {
-    // const revenue = await fetchRevenue();
+    const revenue = await fetchRevenue();
     //   const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
@@ -25,7 +25,7 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue}  />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>

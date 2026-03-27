0fcf5887f9fc641c7a3a66f37262c322fefc8e4a
ammonmerrell <ammonmerrell@outlook.com>
2026-03-27 11:02:55 -0600

    commented out extra text.

--- src/app/ui/revinue-chart.tsx
+++ src/app/ui/revinue-chart.tsx
@@ -25,9 +25,9 @@ export default async function RevenueChart() {
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={` mb-4 text-xl md:text-2xl`}>
+      {/* <h2 className={` mb-4 text-xl md:text-2xl`}>
         Recent Revenue
-      </h2>
+      </h2> */}
       {/* NOTE: Uncomment this code in Chapter 7 */}
 
       <div className="rounded-xl bg-gray-50 p-4">
@@ -57,7 +57,7 @@ export default async function RevenueChart() {
         </div>
         <div className="flex items-center pb-2 pt-6">
           {/* <CalendarIcon className="h-5 w-5 text-gray-500" /> */}
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+          {/* <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3> */}
         </div>
       </div>
     </div>

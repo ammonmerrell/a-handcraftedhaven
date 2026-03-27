8d2feb622973f01bc4da5dd63bc1a276c928f471
ammonmerrell <ammonmerrell@outlook.com>
2026-03-26 18:46:19 -0600

    selected all

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -19,7 +19,7 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue[]>`SELECT name FROM sellers`;
+    const data = await sql<Revenue[]>`SELECT * FROM sellers`;
 
 
     // console.log('Data fetch completed after 3 seconds.');

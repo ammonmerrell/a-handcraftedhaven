2a6ea8ddb16c33717dd2f139c24f4c6b10b99649
ammonmerrell <ammonmerrell@outlook.com>
2026-03-27 11:21:59 -0600

    added sellers and review and product defenitions

--- src/app/lib/definitions.tsx
+++ src/app/lib/definitions.tsx
@@ -2,34 +2,36 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
-export type User = {
+export type sellers = {
   id: string;
   name: string;
   email: string;
   password: string;
+  story: string;
 };
 
-export type Customer = {
+export type review = {
   id: string;
-  name: string;
-  email: string;
-  image_url: string;
-};
-
-export type Invoice = {
-  id: string;
-  customer_id: string;
-  amount: number;
+  product_id: string;
+  rating: '1'|'2'|'3'|'4'|'5';
+  feedback: string;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
 };
 
-export type Revenue = {
-  month: string;
-  revenue: number;
-};
+// export type Invoice = {
+//   id: string;
+//   customer_id: string;
+//   amount: number;
+//   date: string;
+//   // In TypeScript, this is called a string union type.
+//   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+//   status: 'pending' | 'paid';
+// };
+
+// export type Revenue = {
+//   month: string;
+//   revenue: number;
+// };
 
 export type LatestInvoice = {
   id: string;

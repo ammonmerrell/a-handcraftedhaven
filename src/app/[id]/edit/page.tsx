0e10daffac3dbf62866d4e64199a44ee8e03cf40
ammonmerrell <ammonmerrell@outlook.com>
2026-04-02 22:08:38 -0600

    commented out import

--- src/app/[id]/edit/page.tsx
+++ src/app/[id]/edit/page.tsx
@@ -1,4 +1,4 @@
-import Form from '@/app/ui/edit-form';
+// import Form from '@/app/ui/edit-form';
 import Breadcrumbs from '@/app/ui/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
  
@@ -22,7 +22,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      {/* <Form invoice={invoice} customers={customers} /> */}
     </main>
   );
 }

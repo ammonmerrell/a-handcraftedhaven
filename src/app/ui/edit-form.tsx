09ef745ea2d8fbf34d3a504fb08540c290e5dee4
ammonmerrell <ammonmerrell@outlook.com>
2026-04-02 22:26:05 -0600

    commented out form

--- src/app/ui/edit-form.tsx
+++ src/app/ui/edit-form.tsx
@@ -1,24 +1,24 @@
-// 'use client';
+'use client';
 
-// import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-// // import {
-// //   CheckIcon,
-// //   ClockIcon,
-// //   CurrencyDollarIcon,
-// //   UserCircleIcon,
-// // } from '@heroicons/react/24/outline';
-// import Link from 'next/link';
-// import { Button } from '@/app/ui/button';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+// import {
+//   CheckIcon,
+//   ClockIcon,
+//   CurrencyDollarIcon,
+//   UserCircleIcon,
+// } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { Button } from '@/app/ui/button';
 // import { updateInvoice, State } from '@/app/lib/actions';
-// import { useActionState } from 'react';
+import { useActionState } from 'react';
 
-// export default function EditInvoiceForm({
-//   invoice,
-//   customers,
-// }: {
-//   invoice: InvoiceForm;
-//   customers: CustomerField[];
-// }) {
+export default function EditInvoiceForm({
+  invoice,
+  customers,
+}: {
+  invoice: InvoiceForm;
+  customers: CustomerField[];
+}) {
 //   const initialState: State = { message: null, errors: {} };
 //   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 //   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
@@ -126,4 +126,4 @@
 //       </div>
 //     </form>
 //   );
-// }
+}

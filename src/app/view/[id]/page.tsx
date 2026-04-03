7f1f212f430a8b9232877ea7336e27042a11be8a
ammonmerrell <ammonmerrell@outlook.com>
2026-04-03 09:21:47 -0600

    added view/[id] page

---
+++ src/app/view/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { Card } from '@/app/ui/cards'
+import ProductChart from '@/app/ui/revinue-chart';
+import LatestInvoices from '@/app/ui/latest-invoices';
+import {  } from '@/app/page';
+import { fetchProduct } from '@/app/lib/data';
+import { quintessential } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
+ 
+export default async function Page() {
+    const product = await fetchProduct();
+    //   const latestInvoices = await fetchLatestInvoices();
+  return (
+    <main>
+      {/* <h1 className={`${quintessential} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1> */}
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <ProductChart />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  );
+}

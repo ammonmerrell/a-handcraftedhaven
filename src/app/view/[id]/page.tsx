29f24ddc661c71784c32e6dfb6cb88437ff35486
ammonmerrell <ammonmerrell@outlook.com>
2026-04-09 12:21:45 -0600

    made ids go to correct places

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -5,7 +5,7 @@ import {  } from '@/app/page';
 import { fetchItem, fetchProduct } from '@/app/lib/data';
 import { quintessential } from '@/app/ui/fonts';
 import { fetchLatestInvoices } from '@/app/lib/data';
-import ProductChart from '@/app/ui/revinue-chart';
+import ProductChart, { ProductItem } from '@/app/ui/revinue-chart';
 
 // export default async function Page(id: string) {
   
@@ -17,7 +17,7 @@ export default async function Page(props: {params: Promise<{ id: string }> }) {
     // fetchCustomers(),
   ]);
 
-
+  // id="d6e15727-9fe1-4961-8c5b-ea44a9bd81aa"
   // const product = await fetchItem(id);
     //   const latestInvoices = await fetchLatestInvoices();
   return (
@@ -26,7 +26,7 @@ export default async function Page(props: {params: Promise<{ id: string }> }) {
         Dashboard
       </h1> */}
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <ProductChart />
+        <ProductItem id="d6e15727-9fe1-4961-8c5b-ea44a9bd81aa" />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
